e1199b8d87b909833f98eb6d7615d101a5c80d5e
Ahmad <rabieahmad330@gmail.com>
2024-08-29 14:20:07 +0300

    creat handleinputchange function

--- learn-react/src/ToDoList.jsx
+++ learn-react/src/ToDoList.jsx
@@ -5,9 +5,36 @@ function ToDoList(){
 
     const[tasks, setTasks] = useState([])
     const[newTask, setNewTask] = useState([])
- 
+    
+
+    function handleInputChnage(event){
+        setNewTask(event.target.value); 
+    }
+
+    function addTask(){
+
+    }
+
+    function deleteTask(index){
+
+    }
+
+    function moveTaskUp(index){
+
+    }
+
+    function moveTaskDown(index){
+        
+    }
     return(
-        <></>
+        <div className="to-do-list">
+
+            <h1>To-Do-List</h1>
+
+            <div>
+                <input type="text" placeholder="Enter a task..." value={newTask} onChange={handleInputChnage}/>
+            </div>
+        </div>
     );
 }
 export default ToDoList
